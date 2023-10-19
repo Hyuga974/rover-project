cc10b499263b96764e83b1d9c37d04af4101eb2c
Hyuga974 <tamashiyami22@gmail.com>
2023-10-19 15:25:40 +0200

    add abstract class Planet (like an interface)

---
+++ Models/planetInterface.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class PlanetInterface(ABC):
+    @abstractmethod
+    def __init__(self, size_x, size_y, obstacles=None):
+        pass
+    
+    @abstractmethod
+    def check_limit_planet(self, position):
+        pass
+
+    @abstractmethod
+    def is_obstacle_at_position(self, position) :
+        pass
